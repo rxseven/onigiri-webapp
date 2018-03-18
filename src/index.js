b09e9074f6a0e5769e982fd581c3c6cb50893318
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-18 22:36:12 +0700

    Create App component

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './components/App';
+
 // Render React element into the DOM
-ReactDOM.render(<div>React boilerplate</div>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
