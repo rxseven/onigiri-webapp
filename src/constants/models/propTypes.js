e7eb1cecad0b4e5b797b4fae5ddcc9bda81e0284
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-18 23:25:15 +0700

    Add asynchronous objects to propTypes models

--- src/constants/models/propTypes.js
+++ src/constants/models/propTypes.js
@@ -2,4 +2,32 @@
 import PropTypes from 'prop-types';
 
 // Prop types
-export default {};
+export default {
+  model: {
+    asynchronous: PropTypes.shape({
+      error: PropTypes.objectOf(PropTypes.string),
+      loading: PropTypes.bool
+    })
+  },
+  pattern: {
+    asynchronous: props => ({
+      state: PropTypes.shape({
+        ui: PropTypes.shape({
+          asynchronous: PropTypes.shape({
+            error: PropTypes.objectOf(PropTypes.string),
+            loading: PropTypes.bool
+          })
+        })
+      })
+    })
+  },
+  wrapper: {
+    asynchronous: props => ({
+      state: PropTypes.shape({
+        ui: PropTypes.shape({
+          asynchronous: PropTypes.shape({ ...props })
+        })
+      })
+    })
+  }
+};
