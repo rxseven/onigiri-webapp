042d23b80a0e8be9b80c7cd5f048900f7da08d11
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-18 23:53:06 +0700

    Create reducer for root screens node

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import { combineReducers } from 'redux';
 
 // Reducers
 import dataReducer from '../data/reducer';
+import screensReducer from '../screens/reducer';
 
 // Combine reducers
 const reducer = combineReducers({
-  data: dataReducer
+  data: dataReducer,
+  screens: screensReducer
 });
 
 // Module exports
