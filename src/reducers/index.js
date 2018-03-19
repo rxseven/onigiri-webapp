fec49af6c9a9d4404eb634ba827ecde176a61091
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-19 16:05:06 +0700

    Configure reducer for form node

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 // Module dependencies
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 // Reducers
 import dataReducer from '../data/reducer';
@@ -8,6 +9,7 @@ import screensReducer from '../screens/reducer';
 // Combine reducers
 const reducer = combineReducers({
   data: dataReducer,
+  form: formReducer,
   screens: screensReducer
 });
 
