a7a7b934c058b1847b9e61405280373415c5edc9
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-19 11:47:53 +0700

    Configure router structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 // Module dependencies
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { LastLocationProvider } from 'react-router-last-location';
+
+import Routes from './Routes';
 
 // Component
-const App = () => <div>App component</div>;
+const App = () => (
+  <Router>
+    <LastLocationProvider>
+      <Routes />
+    </LastLocationProvider>
+  </Router>
+);
 
 // Module exports
 export default hot(module)(App);
