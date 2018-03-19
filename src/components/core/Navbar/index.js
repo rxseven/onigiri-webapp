6bee477e10a882ec4f6638387d27cfac7467b1b2
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-19 20:02:00 +0700

    Add grid container to Navbar

--- src/components/core/Navbar/index.js
+++ src/components/core/Navbar/index.js
@@ -1,8 +1,14 @@
 // Module dependencies
 import React from 'react';
 
+import { Container } from '../../shared/base/Grid';
+
 // Component
-const Navbar = () => <nav>Navbar component</nav>;
+const Navbar = () => (
+  <nav className="navbar navbar-dark bg-dark fixed-top">
+    <Container>Navbar component</Container>
+  </nav>
+);
 
 // Module exports
 export default Navbar;
