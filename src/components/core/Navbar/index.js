62ddce70dedc4464c9b270c4807339f702b12163
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-19 20:08:10 +0700

    Convert Navbar component to ES6 class based

--- src/components/core/Navbar/index.js
+++ src/components/core/Navbar/index.js
@@ -1,14 +1,18 @@
 // Module dependencies
-import React from 'react';
+import React, { Component } from 'react';
 
 import { Container } from '../../shared/base/Grid';
 
 // Component
-const Navbar = () => (
-  <nav className="navbar navbar-dark bg-dark fixed-top">
-    <Container>Navbar component</Container>
-  </nav>
-);
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-dark fixed-top">
+        <Container>Navbar component</Container>
+      </nav>
+    );
+  }
+}
 
 // Module exports
 export default Navbar;
