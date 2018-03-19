33350b42c505ebfeee175943441b48bd56ccaf14
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-19 11:43:45 +0700

    Create Routes component

---
+++ src/components/Routes.js
@@ -0,0 +1,9 @@
+// Module dependencies
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+// Component
+const Routes = () => <Switch>Routes component</Switch>;
+
+// Module exports
+export default Routes;
