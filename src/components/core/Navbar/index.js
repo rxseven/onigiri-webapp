abe82478cea00dff33b25edeb34302f8e5a82a9a
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-19 20:24:17 +0700

    Add branding & nav link to Navbar

--- src/components/core/Navbar/index.js
+++ src/components/core/Navbar/index.js
@@ -1,14 +1,32 @@
 // Module dependencies
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import { Container } from '../../shared/base/Grid';
 
+// Constants
+import PATHS from '../../../constants/router/paths';
+
+// Peer dependencies
+import styles from './styles.scss';
+
 // Component
 class Navbar extends Component {
+  // Render brand
+  renderBrand = () => (
+    <Link className="navbar-brand" to={PATHS.root}>
+      <div className={styles.logo}>
+        <span className={styles.image} />
+        <span>おにぎり</span>
+      </div>
+    </Link>
+  );
+
+  // Render component
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark fixed-top">
-        <Container>Navbar component</Container>
+        <Container>{this.renderBrand()}</Container>
       </nav>
     );
   }
