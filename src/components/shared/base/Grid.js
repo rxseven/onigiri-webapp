71871c264b3281bbda2019f6b63938e41ce22f95
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-19 18:52:06 +0700

    Create grid Column component

--- src/components/shared/base/Grid.js
+++ src/components/shared/base/Grid.js
@@ -6,6 +6,10 @@ import React from 'react';
 
 // Declare prop types and default props
 const propTypes = exact({
+  column: {
+    children: PropTypes.node.isRequired,
+    size: PropTypes.string
+  },
   container: {
     children: PropTypes.node.isRequired
   },
@@ -16,10 +20,16 @@ const propTypes = exact({
 });
 
 const defaultProps = {
+  column: {
+    size: 'col'
+  },
   row: {
     alignment: null
   }
 };
+
+// Column
+export const Column = ({ children, size }) => <div className={size}>{children}</div>;
 
 // Container
 export const Container = ({ children }) => <div className="container">{children}</div>;
@@ -30,7 +40,9 @@ export const Row = ({ alignment, children }) => (
 );
 
 // Specify prop types and default values for props
+Column.propTypes = propTypes.column;
 Container.propTypes = propTypes.container;
 Row.propTypes = propTypes.row;
 
+Column.defaultProps = defaultProps.column;
 Row.defaultProps = defaultProps.row;
