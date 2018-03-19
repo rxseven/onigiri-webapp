e40d299299c159df9bba815894f98c781dc9dbe1
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-19 14:23:02 +0700

    Create ListGroup component

---
+++ src/components/shared/base/ListGroup/index.js
@@ -0,0 +1,69 @@
+// Module dependencies
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+import exact from 'prop-types-exact';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// Styles
+import styles from './styles.scss';
+
+// Declare prop types and default props
+const propTypes = exact({
+  container: {
+    children: PropTypes.node.isRequired,
+    margin: PropTypes.string
+  },
+  headline: {
+    children: PropTypes.node.isRequired
+  },
+  item: {
+    children: PropTypes.node.isRequired,
+    link: PropTypes.object.isRequired
+  },
+  text: {
+    children: PropTypes.node.isRequired
+  }
+});
+
+const defaultProps = {
+  container: {
+    margin: null
+  }
+};
+
+// List group container
+export const ListGroup = ({ children, margin }) => {
+  // Configuration
+  const baseClass = 'list-group';
+
+  // View
+  return <div className={cx(baseClass, margin)}>{children}</div>;
+};
+
+// List group headline
+export const ListGroupHL = ({ children }) => <h5 className={styles.headline}>{children}</h5>;
+
+// List group item
+export const ListGroupItem = ({ children, link }) => {
+  // Configuration
+  const baseClass = 'list-group-item';
+
+  // View
+  return (
+    <Link className={cx(baseClass, `${baseClass}-action`)} to={link}>
+      {children}
+    </Link>
+  );
+};
+
+// List group text
+export const ListGroupText = ({ children }) => <p className={styles.text}>{children}</p>;
+
+// Specify prop types and default values for props
+ListGroup.propTypes = propTypes.container;
+ListGroupHL.propTypes = propTypes.headline;
+ListGroupItem.propTypes = propTypes.item;
+ListGroupText.propTypes = propTypes.text;
+
+ListGroup.defaultProps = defaultProps.container;
