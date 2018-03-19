199a0d1397505642e7c2188b2e91d25baba0b5f2
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-19 14:45:28 +0700

    Create Form component

--- src/components/shared/base/Form/index.js
+++ src/components/shared/base/Form/index.js
@@ -1,9 +1,36 @@
 // Module dependencies
+import { map } from 'lodash';
 import cx from 'classnames';
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { Field } from 'redux-form';
 
+import { Button, ButtonSet } from '../../base/Buttons';
+import Spinner from '../../base/Spinner';
 import Text from '../../base/Text';
+import Error from '../../extended/Error';
 import Render from '../../helpers/Render';
+
+// Constants
+import PROP_TYPES from '../../../../constants/models/propTypes';
+import STATE_MODELS from '../../../../constants/models/state';
+
+// Declare prop types and default props
+const propTypes = {
+  form: {
+    alert: PropTypes.bool,
+    asynchronous: PROP_TYPES.model.asynchronous,
+    spinner: PropTypes.bool
+  }
+};
+
+const defaultProps = {
+  form: {
+    alert: true,
+    asynchronous: { ...STATE_MODELS.model.asynchronous },
+    spinner: true
+  }
+};
 
 // Form field
 export const FormField = ({
@@ -43,3 +70,64 @@ export const FormField = ({
     </div>
   );
 };
+
+// Form
+export class Form extends Component {
+  // Submit handler
+  onSubmit = (values) => {
+    this.props.submitFunction(values, this.props.submitCallback);
+  };
+
+  // Render field
+  renderField = () =>
+    // Create Field array of values
+    map(this.props.fields, ({
+      helper, label, name, type
+    }, index) => (
+      <Field
+        component={FormField}
+        helper={helper}
+        key={`field-0${index}`}
+        label={label}
+        name={name}
+        type={type}
+      />
+    ));
+
+  // Render a component
+  render() {
+    // Variables
+    const {
+      alert,
+      asynchronous: { error, loading },
+      cancelButton,
+      handleSubmit,
+      pristine,
+      spinner,
+      submitButton
+    } = this.props;
+
+    // View
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        {this.renderField()}
+        <Render condition={alert && error}>
+          <Error alert={error} />
+        </Render>
+        <ButtonSet>
+          {cancelButton}
+          <Button disabled={pristine || (spinner && loading)} styles="primary" type="submit">
+            {submitButton}
+          </Button>
+          <Render condition={spinner && loading}>
+            <Spinner />
+          </Render>
+        </ButtonSet>
+      </form>
+    );
+  }
+}
+
+// Specify prop types and default values for props
+Form.propTypes = propTypes.form;
+Form.defaultProps = defaultProps.form;
