5656f758d8e5955ad8471981f6f51596bb4b680e
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-19 10:29:25 +0700

    Add Regex helper method to validate HTTP protocol

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -3,6 +3,13 @@ import urlRegex from 'url-regex';
 
 // Pattern validation
 export default {
+  http: (string) => {
+    if (string && !/^(http|https):\/\//i.test(string)) {
+      return 'Must begin with http:// or https://';
+    }
+
+    return false;
+  },
   url: (string) => {
     if (string && !urlRegex({ exact: true }).test(string)) {
       return 'Invalid web address';
