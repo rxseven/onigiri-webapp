87b0fefe27a42c0db1bd79dc5cee6b14a16b9185
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-19 16:08:00 +0700

    Add conditional logic to handle state changes in response to modal actions

--- src/data/interfaces/modal/reducer.js
+++ src/data/interfaces/modal/reducer.js
@@ -1,2 +1,25 @@
+// Actions
+import { MODAL_CLOSE, MODAL_OPEN } from './actions';
+
+// Initial state
+const initialState = {
+  isOpen: false
+};
+
 // Reducer
-export default (state = null, action) => state;
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case MODAL_CLOSE:
+      return {
+        ...state,
+        isOpen: false
+      };
+    case MODAL_OPEN:
+      return {
+        ...state,
+        isOpen: true
+      };
+    default:
+      return state;
+  }
+};
