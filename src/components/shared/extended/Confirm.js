f85518f3ed90e814a016aeefb5274164cce6888f
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-19 14:32:16 +0700

    Create Confirm component

---
+++ src/components/shared/extended/Confirm.js
@@ -0,0 +1,69 @@
+// Module dependencies
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import { Button } from '../base/Buttons';
+import { Modal, ModalBody, ModalFooter } from '../base/Modal';
+import Spinner from '../base/Spinner';
+import Error from '../extended/Error';
+import Render from '../helpers/Render';
+
+// Constants
+import PROP_TYPES from '../../../constants/models/propTypes';
+import STATE_MODELS from '../../../constants/models/state';
+
+// Declare prop types and default props
+const propTypes = {
+  alert: PropTypes.bool,
+  asynchronous: PROP_TYPES.model.asynchronous,
+  spinner: PropTypes.bool
+};
+
+const defaultProps = {
+  alert: true,
+  asynchronous: { ...STATE_MODELS.model.asynchronous },
+  spinner: true
+};
+
+// Component
+const Confirm = (props) => {
+  // Variables
+  const {
+    alert,
+    asynchronous: { error, loading },
+    buttonCancel,
+    buttonConfirm,
+    children,
+    onClose,
+    onConfirm,
+    spinner
+  } = props;
+
+  // View
+  return (
+    <Modal {...props}>
+      <ModalBody>{children}</ModalBody>
+      <ModalFooter>
+        <Render condition={spinner && loading}>
+          <Spinner />
+        </Render>
+        <Render condition={alert && error}>
+          <Error alert={error} />
+        </Render>
+        <Button disabled={loading} handler={() => onClose()} styles="secondary">
+          {buttonCancel}
+        </Button>
+        <Button disabled={loading} handler={onConfirm} styles="danger">
+          {buttonConfirm}
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
+};
+
+// Specify prop types and default values for props
+Confirm.propTypes = propTypes;
+Confirm.defaultProps = defaultProps;
+
+// Module exports
+export default Confirm;
