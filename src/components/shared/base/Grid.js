f80530ca949e93f88288411b6fadfbf33abdcb60
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-19 18:50:20 +0700

    Create grid Row component

--- src/components/shared/base/Grid.js
+++ src/components/shared/base/Grid.js
@@ -1,17 +1,36 @@
 // Module dependencies
+import cx from 'classnames';
 import PropTypes from 'prop-types';
 import exact from 'prop-types-exact';
 import React from 'react';
 
-// Declare prop types
+// Declare prop types and default props
 const propTypes = exact({
   container: {
+    children: PropTypes.node.isRequired
+  },
+  row: {
+    alignment: PropTypes.string,
     children: PropTypes.node.isRequired
   }
 });
 
+const defaultProps = {
+  row: {
+    alignment: null
+  }
+};
+
 // Container
 export const Container = ({ children }) => <div className="container">{children}</div>;
 
-// Specify prop types
+// Row
+export const Row = ({ alignment, children }) => (
+  <div className={cx('row', alignment)}>{children}</div>
+);
+
+// Specify prop types and default values for props
 Container.propTypes = propTypes.container;
+Row.propTypes = propTypes.row;
+
+Row.defaultProps = defaultProps.row;
