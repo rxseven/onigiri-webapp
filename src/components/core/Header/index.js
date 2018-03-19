8e62fd9dee441ce01e65d068efeb75e2d3808f77
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-19 19:21:16 +0700

    Replace entire Header's content with Navbar component

--- src/components/core/Header/index.js
+++ src/components/core/Header/index.js
@@ -1,8 +1,10 @@
 // Module dependencies
 import React from 'react';
 
+import Navbar from '../Navbar';
+
 // Component
-const Header = () => <header>Header component</header>;
+const Header = () => <Navbar />;
 
 // Module exports
 export default Header;
