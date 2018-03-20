35d67fd64c8d4aea86116055f133a96913d34655
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-20 21:45:01 +0700

    Create reducer for screens.users.profile node

--- src/screens/Users/reducer.js
+++ src/screens/Users/reducer.js
@@ -2,11 +2,13 @@
 import { combineReducers } from 'redux';
 
 // Reducers
+import profileReducer from './screens/Profile/reducer';
 import signinReducer from './screens/SignIn/reducer';
 import signupReducer from './screens/SignUp/reducer';
 
 // Combine reducers
 export default combineReducers({
+  profile: profileReducer,
   signin: signinReducer,
   signup: signupReducer
 });
