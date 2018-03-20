eb1254a52dcf0660d76aabdcc947f7caa37bccc2
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-20 11:09:04 +0700

    Configure state shape under data.session node

--- src/data/session/reducer.js
+++ src/data/session/reducer.js
@@ -1,2 +1,21 @@
+// Initial state
+const initialState = {
+  authorization: false,
+  verifying: false,
+  user: null
+};
+
+// Data model
+const dataModel = data => ({
+  authorization: true,
+  verifying: false,
+  user: {
+    id: data.id,
+    email: data.email,
+    name: data.name,
+    photo: data.photo
+  }
+});
+
 // Reducer
-export default (state = null, action) => state;
+export default (state = initialState, action) => state;
