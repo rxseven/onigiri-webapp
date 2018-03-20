f7e3734df909bbdf60c2be314f9a39fdb653cbfb
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-20 19:50:12 +0700

    Create reducer for screens.users.signin node

--- src/screens/Users/reducer.js
+++ src/screens/Users/reducer.js
@@ -2,9 +2,11 @@
 import { combineReducers } from 'redux';
 
 // Reducers
+import signinReducer from './screens/SignIn/reducer';
 import signupReducer from './screens/SignUp/reducer';
 
 // Combine reducers
 export default combineReducers({
+  signin: signinReducer,
   signup: signupReducer
 });
