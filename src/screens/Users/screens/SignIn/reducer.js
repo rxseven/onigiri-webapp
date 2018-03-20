2a8851a2b03f92cd01d99e37f08fdc49e7cab947
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-20 20:06:47 +0700

    Add conditional logic to handle state changes in response to sign-in actions

--- src/screens/Users/screens/SignIn/reducer.js
+++ src/screens/Users/screens/SignIn/reducer.js
@@ -1,6 +1,9 @@
 // Module dependencies
 import { combineReducers } from 'redux';
 
+// Actions
+import { SIGNIN, SIGNIN_FAILURE, SIGNIN_SUCCESS } from '../../../../data/session/actions';
+
 // Constants
 import STATE_MODELS from '../../../../constants/models/state';
 
@@ -10,7 +13,32 @@ const initialState = {
 };
 
 // Asynchronous reducer
-const asyncReducer = (state = initialState, action) => state;
+const asyncReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SIGNIN:
+      return {
+        ...state,
+        post: {
+          ...initialState.post,
+          loading: true
+        }
+      };
+    case SIGNIN_FAILURE:
+      return {
+        ...state,
+        post: {
+          ...initialState.post,
+          error: action.payload
+        }
+      };
+    case SIGNIN_SUCCESS:
+      return {
+        ...initialState
+      };
+    default:
+      return state;
+  }
+};
 
 // UI reducer
 const uiReducer = combineReducers({
