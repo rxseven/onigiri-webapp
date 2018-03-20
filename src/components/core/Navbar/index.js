b48dbc9d9fd87066c0c0ea6731d89612f7e41601
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-20 19:37:58 +0700

    Add functionality to sign-out user

--- src/components/core/Navbar/index.js
+++ src/components/core/Navbar/index.js
@@ -2,9 +2,11 @@
 import cx from 'classnames';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link, NavLink } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+import { Link, NavLink, withRouter } from 'react-router-dom';
 import Dropdown, { DropdownContent, DropdownTrigger } from 'react-simple-dropdown';
 
+import { signOut } from '../../../data/session/actions';
 import { getSession } from '../../../data/session/reducer';
 
 import Avatar from '../../shared/base/Avatar';
@@ -19,6 +21,21 @@ import styles from './styles.scss';
 
 // Component
 class Navbar extends Component {
+  // Sign-out handler
+  onSignout = (event) => {
+    // Prevent a browser from being refreshed
+    event.preventDefault();
+
+    // Sign out the current user
+    this.props.actions.auth.signOut(() => {
+      // TODO: 1. Create SignIn screen
+      // TODO: 2. Navigate to sign-in screen after the event has been emitted
+
+      // FIXME: Replace this temporary implementation with TODO No.2
+      this.props.history.push({ pathname: PATHS.users.signup });
+    });
+  };
+
   // Dropdown handler
   onDropdownClick = () => {
     this.refs.dropdown.hide();
@@ -89,8 +106,15 @@ const mapStateToProps = state => ({
   }
 });
 
+// Map dispatch to props
+const mapDispatchToProps = dispatch => ({
+  actions: {
+    auth: bindActionCreators({ signOut }, dispatch)
+  }
+});
+
 // Connect component to application state
-const container = connect(mapStateToProps)(Navbar);
+const container = withRouter(connect(mapStateToProps, mapDispatchToProps)(Navbar));
 
 // Module exports
 export default container;
