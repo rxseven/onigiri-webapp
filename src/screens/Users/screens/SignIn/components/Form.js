a88cb6aa254e3dd154c4f0a041ff882f21894746
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-20 20:29:44 +0700

    Configure basic form configuration

--- src/screens/Users/screens/SignIn/components/Form.js
+++ src/screens/Users/screens/SignIn/components/Form.js
@@ -1,12 +1,18 @@
 // Module dependencies
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+
+import { Form } from '../../../../../components/shared/base/Form';
+
+// Peer dependencies
+import FIELDS from '../constants/fields';
 
 // Component
 class SignInForm extends Component {
   render() {
-    return <div>Form component</div>;
+    return <Form {...this.props} fields={FIELDS} submitButton="Sign in" />;
   }
 }
 
-// Module export
-export default SignInForm;
+// Configure Redux Form
+export default reduxForm({ form: 'signin' })(SignInForm);
