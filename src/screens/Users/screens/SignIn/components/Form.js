ba7a5043e53ce797d125e036f733f6e2a471690b
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-20 20:25:27 +0700

    Create Form component under SignIn screen

---
+++ src/screens/Users/screens/SignIn/components/Form.js
@@ -0,0 +1,12 @@
+// Module dependencies
+import React, { Component } from 'react';
+
+// Component
+class SignInForm extends Component {
+  render() {
+    return <div>Form component</div>;
+  }
+}
+
+// Module export
+export default SignInForm;
--- src/screens/Users/screens/SignIn/index.js
+++ src/screens/Users/screens/SignIn/index.js
@@ -5,6 +5,9 @@ import { Body, Document, Head, Title } from '../../../../components/shared/base/
 import { FormHL } from '../../../../components/shared/base/Form';
 import Layout from '../../../Users/components/Layout';
 
+// Peer dependencies
+import Form from './components/Form';
+
 // Component
 const SignIn = () => (
   <Document>
@@ -14,6 +17,7 @@ const SignIn = () => (
     <Body>
       <Layout>
         <FormHL>Sign in to Onigiri</FormHL>
+        <Form />
       </Layout>
     </Body>
   </Document>
