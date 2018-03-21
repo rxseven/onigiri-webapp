19b0da60d6af3c6c2690799bd9315eff73245252
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-21 15:47:51 +0700

    Create reducer for data.features.payments node

--- src/data/features/reducer.js
+++ src/data/features/reducer.js
@@ -1,2 +1,10 @@
-// Reducer
-export default (state = null, action) => state;
+// Module dependencies
+import { combineReducers } from 'redux';
+
+// Reducers
+import paymentsReducer from './payments/reducer';
+
+// Combine reducers
+export default combineReducers({
+  payments: paymentsReducer
+});
