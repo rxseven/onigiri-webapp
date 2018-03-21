f2d471e3f9e1f716a5e96037878b36dd99042e0f
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-21 10:43:46 +0700

    Provide node state & actions to UI components

--- src/screens/Users/screens/Profile/index.js
+++ src/screens/Users/screens/Profile/index.js
@@ -1,11 +1,32 @@
 // Module dependencies
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 // Peer dependencies
+import * as profileActions from './data/profile/actions';
+import { getProfile } from './data/profile/reducer';
+import { getUI } from './reducer';
 import UI from './UI';
 
+// Map state to props
+const mapStateToProps = state => ({
+  state: {
+    data: {
+      profile: getProfile(state)
+    },
+    ui: getUI(state)
+  }
+});
+
+// Map dispatch to props
+const mapDispatchToProps = dispatch => ({
+  actions: {
+    profile: bindActionCreators(profileActions, dispatch)
+  }
+});
+
 // Connect component to application state
-const container = connect(null)(UI);
+const container = connect(mapStateToProps, mapDispatchToProps)(UI);
 
 // Module exports
 export default container;
