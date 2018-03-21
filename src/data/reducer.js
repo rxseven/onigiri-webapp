0ada74e07babf2a98b5303a924308596794d694f
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-21 14:09:17 +0700

    Create reducer for data.credits node

--- src/data/reducer.js
+++ src/data/reducer.js
@@ -2,11 +2,13 @@
 import { combineReducers } from 'redux';
 
 // Reducers
+import creditsReducer from './credits/reducer';
 import interfacesReducer from './interfaces/reducer';
 import sessionReducer from './session/reducer';
 
 // Combine reducers
 export default combineReducers({
+  credits: creditsReducer,
   interfaces: interfacesReducer,
   session: sessionReducer
 });
