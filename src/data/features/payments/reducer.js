f0a2e9ddcb2cedb526efcabe276553640a1cf3a0
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-21 19:33:13 +0700

    Add selector to get asynchronous state from data.features.payments node

--- src/data/features/payments/reducer.js
+++ src/data/features/payments/reducer.js
@@ -60,3 +60,6 @@ const getNode = state => state.data.features.payments;
 
 // Get UI state
 export const getUI = createSelector(getNode, node => node.ui);
+
+// Get asynchronous state
+export const getAsync = createSelector(getNode, node => node.ui.asynchronous);
