e6e13ef3a45602d62cfb2336e486c7acc4738014
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-21 14:33:31 +0700

    Add conditional logic to handle state changes in response to getting user's credits actions

--- src/data/credits/reducer.js
+++ src/data/credits/reducer.js
@@ -1,8 +1,33 @@
+// Actions
+import { CREDITS_GET, CREDITS_GET_FAILURE, CREDITS_GET_SUCCESS } from './actions';
+
 // Initial state
 const initialState = {
   balance: null,
   lastCheckout: null
 };
 
+// Data model
+const dataModel = data => ({
+  balance: data.balance,
+  lastCheckout: data.lastCheckout
+});
+
 // Reducer
-export default (state = initialState, action) => state;
+export default (state = initialState, action) => {
+  switch (action.type) {
+    // Get credits
+    case CREDITS_GET:
+    case CREDITS_GET_FAILURE:
+      return state;
+    case CREDITS_GET_SUCCESS:
+      return {
+        ...state,
+        ...dataModel(action.payload)
+      };
+
+    // Default
+    default:
+      return state;
+  }
+};
