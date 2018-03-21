20a4f7c89a686041600982decc338a22ab28b3de
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-21 19:55:38 +0700

    Configure Stripe Checkout

--- src/components/features/Payments/UI.js
+++ src/components/features/Payments/UI.js
@@ -1,8 +1,19 @@
 // Module dependencies
 import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
 
 // Component
-const UI = () => <div>Payment component</div>;
+const UI = ({ actions, callback, state }) => (
+  <StripeCheckout
+    amount={500}
+    currency="USD"
+    description="$5.00 for 5 survey credits"
+    name="Onigiri"
+    panelLabel="Add Credits"
+    stripeKey={process.env.REACT_APP_STRIPE_KEY}
+    token={token => actions.payments.checkout(token, callback)}
+  />
+);
 
 // Module exports
 export default UI;
