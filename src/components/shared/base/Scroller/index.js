6ca0124dd496c8ae6afdf6ee0c1836af3804864e
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-22 14:41:13 +0700

    Configure basic component options & add infinite scrolling plugin

--- src/components/shared/base/Scroller/index.js
+++ src/components/shared/base/Scroller/index.js
@@ -1,10 +1,57 @@
 // Module dependencies
 import React, { Component } from 'react';
+import InfiniteScroll from 'react-infinite-scroller';
+
+// Constants
+const INITIAL_STATE = {
+  current: 0,
+  more: true,
+  next: 1,
+  query: null,
+  total: 0
+};
 
 // Scroller container
 export class Scroller extends Component {
+  // Constructor
+  constructor(props) {
+    super(props);
+
+    // Configuration
+    const pagination = props.state.pagination ? props.state.pagination : null;
+
+    // Component properties
+    this.isMouthing = true;
+
+    // Initial state
+    this.state = { ...INITIAL_STATE, ...pagination };
+  }
+
+  // After a component is mounted...
+  componentDidMount() {
+    // Update mouthing state
+    this.isMouthing = true;
+  }
+
+  // Before a component is unmounted and destroyed...
+  componentWillUnmount() {
+    // Update mouthing state
+    this.isMouthing = false;
+  }
+
+  // Render a component
   render() {
-    return <div>Scroller component</div>;
+    return (
+      <InfiniteScroll
+        hasMore={this.state.more}
+        loader={<div key={0}>Loading...</div>}
+        loadMore={undefined}
+        pageStart={1}
+        threshold={100}
+      >
+        {this.props.children}
+      </InfiniteScroll>
+    );
   }
 }
 
