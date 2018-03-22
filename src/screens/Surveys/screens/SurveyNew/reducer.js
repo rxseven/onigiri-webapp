ff10d500bb151d5547ba2d05d483c0d383780378
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-22 09:21:09 +0700

    Configure state shape under screens.surveys.new node

--- src/screens/Surveys/screens/SurveyNew/reducer.js
+++ src/screens/Surveys/screens/SurveyNew/reducer.js
@@ -1,2 +1,18 @@
-// Reducer
-export default (state = null, action) => state;
+// Module dependencies
+import { combineReducers } from 'redux';
+
+// Initial state
+const initialState = null;
+
+// Asynchronous reducer
+const asyncReducer = (state = initialState, action) => state;
+
+// UI reducer
+const uiReducer = combineReducers({
+  asynchronous: asyncReducer
+});
+
+// Combine reducers
+export default combineReducers({
+  ui: uiReducer
+});
