e7bf460490421917cd089a708ff200b903015d59
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-22 09:44:06 +0700

    Add conditional logic to handle state changes in response to submitting survey actions

--- src/screens/Surveys/screens/SurveyNew/reducer.js
+++ src/screens/Surveys/screens/SurveyNew/reducer.js
@@ -1,6 +1,9 @@
 // Module dependencies
 import { combineReducers } from 'redux';
 
+// Actions
+import { SURVEY_CREATE, SURVEY_CREATE_FAILURE, SURVEY_CREATE_SUCCESS } from './actions';
+
 // Constants
 import STATE_MODELS from '../../../../constants/models/state';
 
@@ -10,7 +13,32 @@ const initialState = {
 };
 
 // Asynchronous reducer
-const asyncReducer = (state = initialState, action) => state;
+const asyncReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SURVEY_CREATE:
+      return {
+        ...state,
+        post: {
+          ...initialState.post,
+          loading: true
+        }
+      };
+    case SURVEY_CREATE_FAILURE:
+      return {
+        ...state,
+        post: {
+          ...initialState.post,
+          error: action.payload
+        }
+      };
+    case SURVEY_CREATE_SUCCESS:
+      return {
+        ...initialState
+      };
+    default:
+      return state;
+  }
+};
 
 // UI reducer
 const uiReducer = combineReducers({
