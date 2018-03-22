c885e703f095ac1eb18b13d57bd157353f7d8e0b
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-22 10:35:07 +0700

    Provide error validation to survey form

--- src/screens/Surveys/screens/SurveyNew/components/Form.js
+++ src/screens/Surveys/screens/SurveyNew/components/Form.js
@@ -1,9 +1,12 @@
 // Module dependencies
+import { each } from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 
 import { Form, FormSHL } from '../../../../../components/shared/base/Form';
 import JSXwrapper from '../../../../../components/shared/helpers/JSXwrapper';
+
+import validationHelper from '../../../../../helpers/validation';
 
 // Peer dependencies
 import FIELDS from '../constants/fields';
@@ -32,8 +35,33 @@ class SurveyForm extends Component {
   }
 }
 
+// Error validation rules
+const validate = (values) => {
+  // Initial errors object
+  const errors = {};
+
+  // Validate email addresses
+  errors.recipients = validationHelper.emails(values.recipients);
+
+  // Validate matching landing page's URL
+  errors.landing = validationHelper.url(values.landing);
+
+  // Validate matching landing page's protocol
+  errors.landing = validationHelper.http(values.landing);
+
+  // Iterates over elements of collection and validate value for each element
+  each(FIELDS, ({ name, required }) => {
+    if (required && !values[name]) {
+      errors[name] = 'You must provide a value';
+    }
+  });
+
+  return errors;
+};
+
 // Configure Redux Form
 export default reduxForm({
   form: 'survey',
-  destroyOnUnmount: false
+  destroyOnUnmount: false,
+  validate
 })(SurveyForm);
