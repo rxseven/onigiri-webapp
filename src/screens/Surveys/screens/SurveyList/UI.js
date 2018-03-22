9e89ea6f15a850dadb28efad1934245170f28934
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-22 18:59:26 +0700

    Convert SurveyList’s UI component to ES6 class based

--- src/screens/Surveys/screens/SurveyList/UI.js
+++ src/screens/Surveys/screens/SurveyList/UI.js
@@ -1,6 +1,6 @@
 // Module dependencies
 import cx from 'classnames';
-import React from 'react';
+import React, { Component } from 'react';
 
 import { Body, Document, Head, Title } from '../../../../components/shared/base/Document';
 import Layout from '../../../../components/shared/base/Layout';
@@ -9,16 +9,20 @@ import Layout from '../../../../components/shared/base/Layout';
 import CSS from '../../../../constants/string/css';
 
 // Component
-const UI = () => (
-  <Document>
-    <Head>
-      <Title>Surveys</Title>
-    </Head>
-    <Body>
-      <Layout size={cx(CSS.grid.col.MD12, CSS.grid.col.LG10)}>SurveyList component</Layout>
-    </Body>
-  </Document>
-);
+class UI extends Component {
+  render() {
+    return (
+      <Document>
+        <Head>
+          <Title>Surveys</Title>
+        </Head>
+        <Body>
+          <Layout size={cx(CSS.grid.col.MD12, CSS.grid.col.LG10)}>SurveyList component</Layout>
+        </Body>
+      </Document>
+    );
+  }
+}
 
 // Module exports
 export default UI;
