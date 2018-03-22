983fbbc1e7601cd141ec25f4919960dc10ef4e1b
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-22 20:14:57 +0700

    Add CTA button to create new survey onto Sidebar

--- src/screens/Surveys/screens/SurveyList/components/Sidebar/index.js
+++ src/screens/Surveys/screens/SurveyList/components/Sidebar/index.js
@@ -2,6 +2,12 @@
 import cx from 'classnames';
 import React from 'react';
 
+import { Button } from '../../../../../../components/shared/base/Buttons';
+
+// Constants
+import PATHS from '../../../../../../constants/router/paths';
+import SCREENS from '../../../../../../constants/sizing/screens';
+
 // Peer dependencies
 import styles from './styles.scss';
 
@@ -14,6 +20,19 @@ const Sidebar = ({
   <div className={cx(styles.sidebar, isSticky ? styles.isSticky : false)} style={css}>
     <div className={styles.wrapper}>
       <div className={styles.list}>Menu</div>
+
+      <div className={!isSticky && screenWidth >= SCREENS.small ? 'invisible' : styles.actions}>
+        <Button
+          button="primary"
+          icon="envelope-closed"
+          link={PATHS.surveys.new}
+          size="small"
+          title="Create Survey"
+          type="link"
+        >
+          Create <span className={styles.hidden}>Survey</span>
+        </Button>
+      </div>
     </div>
     <div className={styles.fade} />
   </div>
