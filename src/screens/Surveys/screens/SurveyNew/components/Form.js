c34ff5df8aba94432062955e7efb051eb66b6d5b
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-22 10:31:51 +0700

    Add HTML form & specify basic form configuration

--- src/screens/Surveys/screens/SurveyNew/components/Form.js
+++ src/screens/Surveys/screens/SurveyNew/components/Form.js
@@ -1,12 +1,39 @@
 // Module dependencies
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+
+import { Form, FormSHL } from '../../../../../components/shared/base/Form';
+import JSXwrapper from '../../../../../components/shared/helpers/JSXwrapper';
+
+// Peer dependencies
+import FIELDS from '../constants/fields';
 
 // Component
 class SurveyForm extends Component {
+  // Render content
+  renderContent = () => (
+    <div>
+      <FormSHL>Please enter your entries</FormSHL>
+      <Form
+        {...this.props}
+        alert={false}
+        fields={FIELDS}
+        spinner={false}
+        submitButton="Next"
+        submitCallback={false}
+        submitFunction={undefined}
+      />
+    </div>
+  );
+
+  // Render component
   render() {
-    return <div>Form component</div>;
+    return <JSXwrapper>{this.renderContent()}</JSXwrapper>;
   }
 }
 
-// Module export
-export default SurveyForm;
+// Configure Redux Form
+export default reduxForm({
+  form: 'survey',
+  destroyOnUnmount: false
+})(SurveyForm);
