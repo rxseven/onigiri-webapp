e55bbfcfdf6dd8c86deb72b28b3074af4e094460
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-22 23:18:07 +0700

    Configure plugin for ScrollTop component

--- src/screens/Surveys/screens/SurveyList/components/ScrollTop/index.js
+++ src/screens/Surveys/screens/SurveyList/components/ScrollTop/index.js
@@ -1,8 +1,20 @@
 // Module dependencies
 import React from 'react';
+import ScrollToTop from 'react-scroll-up';
+
+// Configuration
+const options = {
+  showUnder: 160,
+  style: {
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+    right: 0
+  }
+};
 
 // Component
-const ScrollTop = () => <div>ScrollTop component</div>;
+const ScrollTop = () => <ScrollToTop {...options}>ScrollTop</ScrollToTop>;
 
 // Module exports
 export default ScrollTop;
