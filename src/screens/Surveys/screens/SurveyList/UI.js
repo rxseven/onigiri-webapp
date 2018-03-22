6a360674a5364af535e1afb9489192f0aa48c342
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-22 19:04:38 +0700

    Configure & initialize component state to SurveyList screen

--- src/screens/Surveys/screens/SurveyList/UI.js
+++ src/screens/Surveys/screens/SurveyList/UI.js
@@ -8,8 +8,35 @@ import Layout from '../../../../components/shared/base/Layout';
 // Constants
 import CSS from '../../../../constants/string/css';
 
+// Peer dependencies
+import TYPES from './constants/types';
+
 // Component
 class UI extends Component {
+  // Constructor
+  constructor(props) {
+    super(props);
+
+    // Declare default state values
+    const defaultState = { mode: null, query: null, revisit: false };
+
+    // Initialize component state
+    this.state = defaultState;
+  }
+
+  // After a component is mounted...
+  componentDidMount() {
+    // Initialize default configuration
+    this.onInitialize();
+  }
+
+  // Initialize default configuration
+  onInitialize = () => {
+    // Set default list view mode
+    this.setState(() => ({ mode: TYPES.mode.active }));
+  };
+
+  // Render component
   render() {
     return (
       <Document>
