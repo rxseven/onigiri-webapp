0308ba01599f6a22533f7161651ba41da761abba
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-22 20:13:01 +0700

    Scaffold responsive content layout & set dynamic styles to Sidebar component

--- src/screens/Surveys/screens/SurveyList/components/Sidebar/index.js
+++ src/screens/Surveys/screens/SurveyList/components/Sidebar/index.js
@@ -1,8 +1,23 @@
 // Module dependencies
+import cx from 'classnames';
 import React from 'react';
 
+// Peer dependencies
+import styles from './styles.scss';
+
 // Component
-const Sidebar = () => <div>Sidebar component</div>;
+const Sidebar = ({
+  actions, state: {
+    isSticky, css, mode, screenWidth
+  }
+}) => (
+  <div className={cx(styles.sidebar, isSticky ? styles.isSticky : false)} style={css}>
+    <div className={styles.wrapper}>
+      <div className={styles.list}>Menu</div>
+    </div>
+    <div className={styles.fade} />
+  </div>
+);
 
 // Module exports
 export default Sidebar;
