9e736d428024a50993c3c2f63157065c17a3ffd9
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-23 14:28:37 +0700

    Add content section to display additional meta data

--- src/screens/Surveys/screens/SurveyDetails/components/Content.js
+++ src/screens/Surveys/screens/SurveyDetails/components/Content.js
@@ -25,97 +25,123 @@ import Chart from './Chart';
 import Recipients from './Recipients';
 
 // Component
-const Content = ({ actions, state: { data, ui: { asynchronous } } }) => (
-  <JSXwrapper>
-    <Card background={data.completed ? 'light' : 'default'}>
-      <CardHeader>{data.title}</CardHeader>
-      <CardBody>
-        <Chart data={data} />
-        <CardSubtitle options={CSS.margin.MB04}>Statistics</CardSubtitle>
-        <List>
-          <ListItem inline>
-            <ListLabel>Yes</ListLabel>
-            <ListContent>{data.yes}</ListContent>
-          </ListItem>
-          <ListItem inline>
-            <ListLabel>No</ListLabel>
-            <ListContent>{data.no}</ListContent>
-          </ListItem>
-        </List>
+const Content = ({ actions, state: { data, ui: { asynchronous } } }) => {
+  // Variables
+  const { archived, completed } = data;
+  const isActive = !archived && !completed;
+  const isClosed = archived && completed;
 
-        <Render condition={data.lastResponded}>
-          <hr />
-          <CardSubtitle options={CSS.margin.MB04}>Response</CardSubtitle>
+  // View
+  return (
+    <JSXwrapper>
+      <Card background={data.completed ? 'light' : 'default'}>
+        <CardHeader>{data.title}</CardHeader>
+        <CardBody>
+          <Chart data={data} />
+          <CardSubtitle options={CSS.margin.MB04}>Statistics</CardSubtitle>
           <List>
+            <ListItem inline>
+              <ListLabel>Yes</ListLabel>
+              <ListContent>{data.yes}</ListContent>
+            </ListItem>
+            <ListItem inline>
+              <ListLabel>No</ListLabel>
+              <ListContent>{data.no}</ListContent>
+            </ListItem>
+          </List>
+
+          <Render condition={data.lastResponded}>
+            <hr />
+            <CardSubtitle options={CSS.margin.MB04}>Response</CardSubtitle>
+            <List>
+              <ListItem>
+                <ListLabel>Total</ListLabel>
+                <ListContent>{data.yes + data.no}</ListContent>
+              </ListItem>
+              <ListItem>
+                <ListLabel>Recent responded</ListLabel>
+                <ListContent>
+                  {timestampHelper.date(data.lastResponded)},{' '}
+                  {timestampHelper.time(data.lastResponded)}
+                </ListContent>
+              </ListItem>
+            </List>
+          </Render>
+
+          <hr />
+
+          <CardSubtitle options={CSS.margin.MB04}>Email</CardSubtitle>
+          <List>
+            {data.from && (
+              <ListItem>
+                <ListLabel>From</ListLabel>
+                <ListContent>
+                  <Text>{data.from}</Text>
+                </ListContent>
+              </ListItem>
+            )}
             <ListItem>
-              <ListLabel>Total</ListLabel>
-              <ListContent>{data.yes + data.no}</ListContent>
+              <ListLabel>Subject line</ListLabel>
+              <ListContent>{data.subject}</ListContent>
             </ListItem>
             <ListItem>
-              <ListLabel>Recent responded</ListLabel>
+              <ListLabel>Body</ListLabel>
+              <ListContent>{data.body}</ListContent>
+            </ListItem>
+            {data.landing && (
+              <ListItem>
+                <ListLabel>Landing page</ListLabel>
+                <ListContent>
+                  <ExLink icon to={data.landing}>
+                    {data.landing}
+                  </ExLink>
+                </ListContent>
+              </ListItem>
+            )}
+            <ListItem>
+              <ListLabel>Creation date</ListLabel>
               <ListContent>
-                {timestampHelper.date(data.lastResponded)},{' '}
-                {timestampHelper.time(data.lastResponded)}
+                {timestampHelper.date(data.dateSent)}, {timestampHelper.time(data.dateSent)}
               </ListContent>
             </ListItem>
           </List>
-        </Render>
 
-        <hr />
+          <hr />
 
-        <CardSubtitle options={CSS.margin.MB04}>Email</CardSubtitle>
-        <List>
-          {data.from && (
+          <CardSubtitle options={CSS.margin.MB04}>Meta</CardSubtitle>
+          <List>
             <ListItem>
-              <ListLabel>From</ListLabel>
+              <ListLabel>Status</ListLabel>
               <ListContent>
-                <Text>{data.from}</Text>
+                <Text>
+                  {isActive && 'active'}
+                  {archived && 'archived'}
+                  {isClosed && ', '}
+                  {completed && 'completed'}
+                </Text>
               </ListContent>
             </ListItem>
-          )}
-          <ListItem>
-            <ListLabel>Subject line</ListLabel>
-            <ListContent>{data.subject}</ListContent>
-          </ListItem>
-          <ListItem>
-            <ListLabel>Body</ListLabel>
-            <ListContent>{data.body}</ListContent>
-          </ListItem>
-          {data.landing && (
-            <ListItem>
-              <ListLabel>Landing page</ListLabel>
-              <ListContent>
-                <ExLink icon to={data.landing}>
-                  {data.landing}
-                </ExLink>
-              </ListContent>
-            </ListItem>
-          )}
-          <ListItem>
-            <ListLabel>Creation date</ListLabel>
-            <ListContent>
-              {timestampHelper.date(data.dateSent)}, {timestampHelper.time(data.dateSent)}
-            </ListContent>
-          </ListItem>
-        </List>
+          </List>
 
-        <Recipients
-          actions={actions}
-          state={{
-            data: data.recipients,
-            ui: { asynchronous: asynchronous.get.recipients }
-          }}
-        />
-      </CardBody>
-    </Card>
+          <Recipients
+            actions={actions}
+            state={{
+              data: data.recipients,
+              ui: { asynchronous: asynchronous.get.recipients }
+            }}
+          />
+        </CardBody>
+      </Card>
 
-    <Render condition={data.locked}>
-      <Alert type="warning">
-        <Icon name="lock-locked" title="Locked" /> This sample survey is locked from being deleted.
-      </Alert>
-    </Render>
-  </JSXwrapper>
-);
+      <Render condition={data.locked}>
+        <Alert type="warning">
+          <Icon name="lock-locked" title="Locked" /> This sample survey is locked from being
+          deleted.
+        </Alert>
+      </Render>
+    </JSXwrapper>
+  );
+};
 
 // Module exports
 export default Content;
