4d18d70c49e535e525fbfada3294e414b3da9829
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-23 12:09:20 +0700

    Add CTA button to trigger functionality to get recipient list

--- src/screens/Surveys/screens/SurveyDetails/components/Recipients/index.js
+++ src/screens/Surveys/screens/SurveyDetails/components/Recipients/index.js
@@ -1,8 +1,10 @@
 // Module dependencies
 import React from 'react';
 
+import { Button, ButtonSet } from '../../../../../../components/shared/base/Buttons';
 import { CardSubtitle } from '../../../../../../components/shared/base/Card';
 import JSXwrapper from '../../../../../../components/shared/helpers/JSXwrapper';
+import Render from '../../../../../../components/shared/helpers/Render';
 
 // Constants
 import PROP_TYPES from '../../../../../../constants/models/propTypes';
@@ -14,12 +16,30 @@ const propTypes = PROP_TYPES.pattern.asynchronous;
 const defaultProps = STATE_MODELS.pattern.asynchronous;
 
 // Component
-const Recipients = () => (
-  <JSXwrapper>
-    <hr />
-    <CardSubtitle options={CSS.margin.MB04}>Recipients</CardSubtitle>
-  </JSXwrapper>
-);
+const Recipients = ({ actions, state: { data, ui: { asynchronous } } }) => {
+  // Variables
+  const { error, loading } = asynchronous;
+
+  // View
+  return (
+    <JSXwrapper>
+      <hr />
+      <CardSubtitle options={CSS.margin.MB04}>Recipients</CardSubtitle>
+      <Render condition={!data}>
+        <ButtonSet options={error && CSS.margin.MB04}>
+          <Button
+            button="outline-primary"
+            disabled={loading}
+            handler={actions.getRecipients}
+            size="small"
+          >
+            View recipient list
+          </Button>
+        </ButtonSet>
+      </Render>
+    </JSXwrapper>
+  );
+};
 
 // Specify prop types and default values for props
 Recipients.propTypes = propTypes;
