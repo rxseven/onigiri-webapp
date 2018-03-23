1d9ca3859899cbe6e4c8e092326be0c147663ae0
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-23 09:22:33 +0700

    Add actions & action creators to get survey

---
+++ src/screens/Surveys/screens/SurveyDetails/data/survey/actions.js
@@ -0,0 +1,42 @@
+// Module dependencies
+import * as surveysService from '../../../../../../services/surveys';
+
+// Actions
+export const SURVEY_GET = 'Survey/data/SURVEY_GET';
+export const SURVEY_GET_FAILURE = 'Survey/data/SURVEY_GET_FAILURE';
+export const SURVEY_GET_SUCCESS = 'Survey/data/SURVEY_GET_SUCCESS';
+
+// Get survey : Success
+const getSurveySuccess = data => ({
+  type: SURVEY_GET_SUCCESS,
+  payload: data
+});
+
+// Get survey : Failure
+const getSurveyFailure = error => ({
+  type: SURVEY_GET_FAILURE,
+  payload: error.response.data.error
+});
+
+// Get survey : Start (loading)
+export const getSurvey = (id, callback) => async (dispatch) => {
+  try {
+    // 1. Inform a reducer that the request began (loading)
+    dispatch({ type: SURVEY_GET });
+
+    // 2. Get survey
+    // 3. Retrieve data in a response and transform to an appropriate format
+    const { data } = await surveysService.getSurvey(id);
+
+    // 4. Inform a reducer that the request finished successfully
+    dispatch(getSurveySuccess(data));
+
+    // 5. Execute a callback
+    if (callback) {
+      callback();
+    }
+  } catch (error) {
+    // Inform a reducer that the request failed
+    dispatch(getSurveyFailure(error));
+  }
+};
