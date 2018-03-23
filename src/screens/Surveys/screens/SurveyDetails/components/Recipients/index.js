9e00ea65f64510f89d1c646c51719360e86330e0
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-23 12:05:52 +0700

    Scaffold content layout & set styles to Recipients component

--- src/screens/Surveys/screens/SurveyDetails/components/Recipients/index.js
+++ src/screens/Surveys/screens/SurveyDetails/components/Recipients/index.js
@@ -1,16 +1,25 @@
 // Module dependencies
 import React from 'react';
 
+import { CardSubtitle } from '../../../../../../components/shared/base/Card';
+import JSXwrapper from '../../../../../../components/shared/helpers/JSXwrapper';
+
 // Constants
 import PROP_TYPES from '../../../../../../constants/models/propTypes';
 import STATE_MODELS from '../../../../../../constants/models/state';
+import CSS from '../../../../../../constants/string/css';
 
 // Declare prop types and default props
 const propTypes = PROP_TYPES.pattern.asynchronous;
 const defaultProps = STATE_MODELS.pattern.asynchronous;
 
 // Component
-const Recipients = () => <div>Recipients component</div>;
+const Recipients = () => (
+  <JSXwrapper>
+    <hr />
+    <CardSubtitle options={CSS.margin.MB04}>Recipients</CardSubtitle>
+  </JSXwrapper>
+);
 
 // Specify prop types and default values for props
 Recipients.propTypes = propTypes;
