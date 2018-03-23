e73a7ec51704d1c8cb3dd55185522a4bc087061a
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-23 09:26:35 +0700

    Add conditional logic to handle state changes in response to getting survey actions

--- src/screens/Surveys/screens/SurveyDetails/data/survey/reducer.js
+++ src/screens/Surveys/screens/SurveyDetails/data/survey/reducer.js
@@ -1,5 +1,24 @@
+// Actions
+import { SURVEY_GET, SURVEY_GET_FAILURE, SURVEY_GET_SUCCESS } from './actions';
+
 // Initial state
-const initialState = null;
+const initialState = {};
 
 // Reducer
-export default (state = initialState, action) => state;
+export default (state = initialState, action) => {
+  switch (action.type) {
+    // Get survey
+    case SURVEY_GET:
+    case SURVEY_GET_FAILURE:
+      return state;
+    case SURVEY_GET_SUCCESS:
+      return {
+        ...state,
+        ...action.payload
+      };
+
+    // Default
+    default:
+      return state;
+  }
+};
--- src/screens/Surveys/screens/SurveyDetails/reducer.js
+++ src/screens/Surveys/screens/SurveyDetails/reducer.js
@@ -1,6 +1,9 @@
 // Module dependencies
 import { combineReducers } from 'redux';
 
+// Actions
+import { SURVEY_GET, SURVEY_GET_FAILURE, SURVEY_GET_SUCCESS } from './data/survey/actions';
+
 // Reducers
 import dataReducer from './data/reducer';
 
@@ -13,7 +16,45 @@ const initialState = {
 };
 
 // Asynchronous reducer
-const asyncReducer = (state = initialState, action) => state;
+const asyncReducer = (state = initialState, action) => {
+  switch (action.type) {
+    // Get survey
+    case SURVEY_GET:
+      return {
+        ...state,
+        get: {
+          ...state.get,
+          survey: {
+            ...initialState.get.survey,
+            loading: true
+          }
+        }
+      };
+    case SURVEY_GET_FAILURE:
+      return {
+        ...state,
+        get: {
+          ...state.get,
+          survey: {
+            ...initialState.get.survey,
+            error: action.payload
+          }
+        }
+      };
+    case SURVEY_GET_SUCCESS:
+      return {
+        ...state,
+        get: {
+          ...state.get,
+          survey: initialState.get.survey
+        }
+      };
+
+    // Default
+    default:
+      return state;
+  }
+};
 
 // UI reducer
 const uiReducer = combineReducers({
