351acfcf5fb4e92790a16c7ae6ccfcec19bc2efb
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-23 15:59:27 +0700

    Scaffold responsive content layout & set styles to Footer component

--- src/components/core/Footer/index.js
+++ src/components/core/Footer/index.js
@@ -1,17 +1,35 @@
 // Module dependencies
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import { Column, Container, Row } from '../../shared/base/Grid';
+
+// Constants
+import PATHS from '../../../constants/router/paths';
 
 // Peer dependencies
 import styles from './styles.scss';
 
-// Component
+// Link
+const FooterLink = ({ children, to }) => (
+  <NavLink activeClassName={styles.active} to={to}>
+    {children}
+  </NavLink>
+);
+
+// Footer
 const Footer = () => (
   <footer className={styles.wrapper}>
     <Container>
       <Row>
-        <Column>Footer component</Column>
+        <Column>
+          <ul className={styles.navigation}>
+            <li>
+              <FooterLink to={PATHS.root}>Link</FooterLink>
+            </li>
+          </ul>
+          <div className={styles.content}>Content</div>
+        </Column>
       </Row>
     </Container>
   </footer>
