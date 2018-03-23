55a75a22092fd72d12a5cee42acffb80f2c5aa00
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-23 11:57:53 +0700

    Specify prop types and default values for props

--- src/screens/Surveys/screens/SurveyDetails/components/Recipients/index.js
+++ src/screens/Surveys/screens/SurveyDetails/components/Recipients/index.js
@@ -1,8 +1,20 @@
 // Module dependencies
 import React from 'react';
 
+// Constants
+import PROP_TYPES from '../../../../../../constants/models/propTypes';
+import STATE_MODELS from '../../../../../../constants/models/state';
+
+// Declare prop types and default props
+const propTypes = PROP_TYPES.pattern.asynchronous;
+const defaultProps = STATE_MODELS.pattern.asynchronous;
+
 // Component
 const Recipients = () => <div>Recipients component</div>;
+
+// Specify prop types and default values for props
+Recipients.propTypes = propTypes;
+Recipients.defaultProps = defaultProps;
 
 // Module exports
 export default Recipients;
