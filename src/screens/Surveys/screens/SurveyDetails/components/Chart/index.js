cc120efb9d1f5550631b930f8d5a48c2a04ef66d
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-23 10:41:35 +0700

    Create Chart component under SurveyDetails screen

---
+++ src/screens/Surveys/screens/SurveyDetails/components/Chart/index.js
@@ -0,0 +1,8 @@
+// Module dependencies
+import React from 'react';
+
+// Component
+const Chart = () => <div>Chart component</div>;
+
+// Module exports
+export default Chart;
--- src/screens/Surveys/screens/SurveyDetails/components/Content.js
+++ src/screens/Surveys/screens/SurveyDetails/components/Content.js
@@ -1,8 +1,16 @@
 // Module dependencies
 import React from 'react';
 
+// Peer dependencies
+import Chart from './Chart';
+
 // Component
-const Content = () => <div>Content component</div>;
+const Content = () => (
+  <div>
+    Content component
+    <Chart />
+  </div>
+);
 
 // Module exports
 export default Content;
