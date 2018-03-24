441905d17f259ac93be5359219a803778014a796
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-24 13:19:29 +0700

    Add actions & action creators to get landing page URI

---
+++ src/screens/Surveys/screens/SurveyDoorway/data/landing/actions.js
@@ -0,0 +1,42 @@
+// Module dependencies
+import * as surveysService from '../../../../../../services/surveys';
+
+// Actions
+export const LANDING_GET = 'Survey/data/LANDING_GET';
+export const LANDING_GET_FAILURE = 'Survey/data/LANDING_GET_FAILURE';
+export const LANDING_GET_SUCCESS = 'Survey/data/LANDING_GET_SUCCESS';
+
+// Get landing page URI : Success
+const getLandingSuccess = data => ({
+  type: LANDING_GET_SUCCESS,
+  payload: data
+});
+
+// Get landing page URI : Failure
+const getLandingFailure = error => ({
+  type: LANDING_GET_FAILURE,
+  payload: error.response.data.error
+});
+
+// Get landing page URI : Start (loading)
+export const getLanding = (id, callback) => async (dispatch) => {
+  try {
+    // 1. Inform a reducer that the request began (loading)
+    dispatch({ type: LANDING_GET });
+
+    // 2. Get landing page URI
+    // 3. Retrieve data in a response and transform to an appropriate format
+    const { data } = await surveysService.getLanding(id);
+
+    // 4. Inform a reducer that the request finished successfully
+    dispatch(getLandingSuccess(data));
+
+    // 5. Execute a callback
+    if (callback) {
+      callback();
+    }
+  } catch (error) {
+    // Inform a reducer that the request failed
+    dispatch(getLandingFailure(error));
+  }
+};
