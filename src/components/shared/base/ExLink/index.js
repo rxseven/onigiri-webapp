c17eba34f65fd5dfb1db89004c89628aedbd11c0
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-24 15:21:36 +0700

    Update ExLink component with additional props

--- src/components/shared/base/ExLink/index.js
+++ src/components/shared/base/ExLink/index.js
@@ -1,4 +1,5 @@
 // Module dependencies
+import cx from 'lodash';
 import PropTypes from 'prop-types';
 import exact from 'prop-types-exact';
 import React from 'react';
@@ -12,8 +13,10 @@ import styles from './styles.scss';
 
 // Declare prop types and default props
 const propTypes = exact({
+  button: PropTypes.string,
   children: PropTypes.node.isRequired,
   icon: PropTypes.bool,
+  replace: PropTypes.bool,
   to: PropTypes.string.isRequired
 });
 
@@ -22,9 +25,16 @@ const defaultProps = {
 };
 
 // Component
-const ExLink = ({ children, icon, to }) => (
+const ExLink = ({
+  button, children, icon, to, replace
+}) => (
   <JSXwrapper>
-    <a href={to} rel="noopener noreferrer" target="_blank">
+    <a
+      className={cx(button && `btn btn-${button}`)}
+      href={to}
+      rel="noopener noreferrer"
+      target={replace ? '_self' : '_blank'}
+    >
       {children}
       <Render condition={icon}>
         <span className={styles.icon}>
