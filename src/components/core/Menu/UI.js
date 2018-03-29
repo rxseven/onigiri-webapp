a581ffe9ba38267c5c6a18225b06af6e033df2fd
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-29 15:54:54 +0700

    Create MenuLink micro component

--- src/components/core/Menu/UI.js
+++ src/components/core/Menu/UI.js
@@ -1,14 +1,32 @@
 // Module dependencies
+import cx from 'classnames';
 import React, { Component } from 'react';
 import { slide as Menu } from 'react-burger-menu';
-import { withRouter } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
 import { decorator as reduxMenu } from 'redux-burger-menu';
+
+import Icon from '../../shared/base/Icon';
 
 // Constants
 import HTML from '../../../constants/elements/html';
 
+// Peer dependencies
+import styles from './styles.scss';
+
 // Extended Menu component
 const ExMenu = reduxMenu(Menu);
+
+// Link micro component
+const MenuLink = ({
+  children, icon, title, to
+}) => (
+  <NavLink activeClassName={cx('is-active', styles.active)} to={to}>
+    <span className={styles.icon}>
+      <Icon name={icon} title={title} />
+    </span>
+    {children}
+  </NavLink>
+);
 
 // Component
 class UI extends Component {
