6aa78f220dfe8164c6356bdfe6211ecc9d4b7721
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-29 15:37:59 +0700

    Create Menu component

---
+++ src/components/core/Menu/UI.js
@@ -0,0 +1,12 @@
+// Module dependencies
+import React, { Component } from 'react';
+
+// Component
+class UI extends Component {
+  render() {
+    return <div>Menu component</div>;
+  }
+}
+
+// Module exports
+export default UI;
---
+++ src/components/core/Menu/index.js
@@ -0,0 +1,11 @@
+// Module dependencies
+import React from 'react';
+
+// Peer dependencies
+import UI from './UI';
+
+// Component
+const Menu = () => <UI />;
+
+// Module exports
+export default Menu;
