f698110b51ffb2c8f4a24c473eb5a85a25c58c11
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-29 15:48:01 +0700

    Provide node state & actions to UI components

--- src/components/core/Menu/index.js
+++ src/components/core/Menu/index.js
@@ -1,11 +1,22 @@
 // Module dependencies
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 // Peer dependencies
 import UI from './UI';
 
+// Map state to props
+const mapStateToProps = state => ({
+  data: {}
+});
+
+// Map dispatch to props
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators({}, dispatch)
+});
+
 // Connect component to application state
-const container = connect(null)(UI);
+const container = connect(mapStateToProps, mapDispatchToProps)(UI);
 
 // Module exports
 export default container;
