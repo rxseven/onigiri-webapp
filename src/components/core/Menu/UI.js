75391c1b5d07e2a5a21e6f4c8b606a91c5c97c80
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-29 15:43:01 +0700

    Configure off-canvas menu plugin

--- src/components/core/Menu/UI.js
+++ src/components/core/Menu/UI.js
@@ -1,10 +1,32 @@
 // Module dependencies
 import React, { Component } from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import { decorator as reduxMenu } from 'redux-burger-menu';
+
+// Constants
+import HTML from '../../../constants/elements/html';
+
+// Extended Menu component
+const ExMenu = reduxMenu(Menu);
 
 // Component
 class UI extends Component {
   render() {
-    return <div>Menu component</div>;
+    // Options
+    const menuOptions = {
+      customBurgerIcon: false,
+      customCrossIcon: false,
+      outerContainerId: HTML.wrapper,
+      pageWrapId: HTML.body,
+      width: '85%'
+    };
+
+    // View
+    return (
+      <ExMenu {...menuOptions}>
+        <div>Menu component</div>
+      </ExMenu>
+    );
   }
 }
 
