def0e8c54f0b41ddedd21078f813da4428372ca3
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-30 20:50:16 +0700

    Configure state shape under data.interfaces.session node

--- src/data/interfaces/session/reducer.js
+++ src/data/interfaces/session/reducer.js
@@ -1,2 +1,13 @@
-// Reducer
-export default (state = null, action) => state;
+// Module dependencies
+import { combineReducers } from 'redux';
+
+// Initial state
+const initialState = null;
+
+// Asynchronous reducer
+const asyncReducer = (state = initialState, action) => state;
+
+// Combine reducers
+export default combineReducers({
+  asynchronous: asyncReducer
+});
