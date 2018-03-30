f155fa96708e4ec93c8204475d2abb5d7ce33d02
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-30 13:35:42 +0700

    Add demo tips & instruction notes to SignIn screen

--- src/screens/Users/screens/SignIn/index.js
+++ src/screens/Users/screens/SignIn/index.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 
 import { Body, Document, Head, Title } from '../../../../components/shared/base/Document';
 import { FormHL, FormMeta } from '../../../../components/shared/base/Form';
+import Icon from '../../../../components/shared/base/Icon';
+import { Tip, TipHeader } from '../../../../components/shared/base/Tip';
 import Layout from '../../../Users/components/Layout';
 
 // Constants
@@ -25,6 +27,17 @@ const SignIn = () => (
         <FormMeta>
           New to Onigiri? <Link to={PATHS.users.signup}>Create an account</Link>.
         </FormMeta>
+
+        <Tip end>
+          <TipHeader>
+            <Icon name="star" title="Tips" /> Demo tips
+          </TipHeader>
+          <p>Not ready to create a new accout? sure, you can play with the testing account:</p>
+          <p>
+            email: <code>skywalker@rxseven.com</code>
+            <br />password: <code>R2D2s</code>
+          </p>
+        </Tip>
       </Layout>
     </Body>
   </Document>
