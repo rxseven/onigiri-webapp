e678023c6d33b8b44c273dab6c08de0bdf17e120
ME (Mac mini) <rxseven.com@gmail.com>
2018-03-30 20:51:47 +0700

    Add state object to handle asynchronous operation under data.interfaces.session node

--- src/data/interfaces/session/reducer.js
+++ src/data/interfaces/session/reducer.js
@@ -1,8 +1,15 @@
 // Module dependencies
 import { combineReducers } from 'redux';
 
+// Constants
+import STATE_MODELS from '../../../constants/models/state';
+
 // Initial state
-const initialState = null;
+const initialState = {
+  signout: {
+    ...STATE_MODELS.model.asynchronous
+  }
+};
 
 // Asynchronous reducer
 const asyncReducer = (state = initialState, action) => state;
