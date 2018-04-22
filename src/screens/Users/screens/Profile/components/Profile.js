57a0820c00b3186adfb28fd72b284e3bd66ae610
ME (Mac mini) <rxseven.com@gmail.com>
2018-04-22 13:58:12 +0000

    Refactor duplicated code with variables

--- src/screens/Users/screens/Profile/components/Profile.js
+++ src/screens/Users/screens/Profile/components/Profile.js
@@ -8,61 +8,67 @@ import { Card, CardBody, CardHeader } from '../../../../../components/shared/bas
 import styles from '../styles/profile.scss';
 
 // Component
-const Profile = ({ state: { data } }) => (
-  <Card>
-    <CardHeader>Profile</CardHeader>
-    <CardBody>
-      <div className={styles.list}>
-        <div className={styles.item}>
-          <div className={styles.label}>Avatar</div>
-          <div className={styles.content}>
-            <div className={styles.avatar}>
-              <img alt="Avatar" className={styles.photo} src={data.photo.url} />
-              <If condition={data.provider === 'local'}>
-                <a
-                  className={styles.overlay}
-                  href="https://en.gravatar.com/"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <span className={styles.text}>
-                    Change<br /> avatar
-                  </span>
-                </a>
+const Profile = ({ state: { data } }) => {
+  // Variables
+  const isLocal = data.provider === 'local';
+
+  // View
+  return (
+    <Card>
+      <CardHeader>Profile</CardHeader>
+      <CardBody>
+        <div className={styles.list}>
+          <div className={styles.item}>
+            <div className={styles.label}>Avatar</div>
+            <div className={styles.content}>
+              <div className={styles.avatar}>
+                <img alt="Avatar" className={styles.photo} src={data.photo.url} />
+                <If condition={isLocal}>
+                  <a
+                    className={styles.overlay}
+                    href="https://en.gravatar.com/"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <span className={styles.text}>
+                      Change<br /> avatar
+                    </span>
+                  </a>
+                </If>
+              </div>
+              <If condition={isLocal}>
+                <div className={styles.description}>
+                  <p>
+                    Update your avatar through your{' '}
+                    <a href="https://en.gravatar.com/" rel="noopener noreferrer" target="_blank">
+                      Gavatar account
+                    </a>.
+                  </p>
+                </div>
               </If>
             </div>
-            <If condition={data.provider === 'local'}>
-              <div className={styles.description}>
-                <p>
-                  Update your avatar through your{' '}
-                  <a href="https://en.gravatar.com/" rel="noopener noreferrer" target="_blank">
-                    Gavatar account
-                  </a>.
-                </p>
-              </div>
-            </If>
+          </div>
+          <div className={styles.item}>
+            <div className={styles.label}>Name</div>
+            <div className={styles.content}>{`${data.name.firstName} ${data.name.lastName}`}</div>
+          </div>
+          <div className={styles.item}>
+            <div className={styles.label}>Email</div>
+            <div className={styles.content}>{data.email}</div>
+          </div>
+          <div className={styles.item}>
+            <div className={styles.label}>Gender</div>
+            <div className={styles.content}>{data.gender}</div>
+          </div>
+          <div className={cx(styles.item, styles.end)}>
+            <div className={styles.label}>Language</div>
+            <div className={styles.content}>{data.language}</div>
           </div>
         </div>
-        <div className={styles.item}>
-          <div className={styles.label}>Name</div>
-          <div className={styles.content}>{`${data.name.firstName} ${data.name.lastName}`}</div>
-        </div>
-        <div className={styles.item}>
-          <div className={styles.label}>Email</div>
-          <div className={styles.content}>{data.email}</div>
-        </div>
-        <div className={styles.item}>
-          <div className={styles.label}>Gender</div>
-          <div className={styles.content}>{data.gender}</div>
-        </div>
-        <div className={cx(styles.item, styles.end)}>
-          <div className={styles.label}>Language</div>
-          <div className={styles.content}>{data.language}</div>
-        </div>
-      </div>
-    </CardBody>
-  </Card>
-);
+      </CardBody>
+    </Card>
+  );
+};
 
 // Module exports
 export default Profile;
