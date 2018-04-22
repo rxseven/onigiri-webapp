a3af1ea7b67f479cb0add42128685425008712a5
ME (Mac mini) <rxseven.com@gmail.com>
2018-04-22 08:41:02 +0000

    Provide node state & actions to Facebook’s UI component

--- src/components/features/OAuth/Facebook/index.js
+++ src/components/features/OAuth/Facebook/index.js
@@ -1,11 +1,31 @@
 // Module dependencies
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { oauthFacebook, oauthFailure, oauthRequest } from '../../../../data/session/actions';
+import { getSession } from '../../../../data/session/reducer';
 
 // Peer dependencies
 import UI from './UI';
 
+// Map state to props
+const mapStateToProps = state => ({
+  state: {
+    data: {
+      session: getSession(state)
+    }
+  }
+});
+
+// Map dispatch to props
+const mapDispatchToProps = dispatch => ({
+  actions: {
+    auth: bindActionCreators({ oauthFacebook, oauthFailure, oauthRequest }, dispatch)
+  }
+});
+
 // Connect component to application state
-const container = connect(null)(UI);
+const container = connect(mapStateToProps, mapDispatchToProps)(UI);
 
 // Module exports
 export default container;
