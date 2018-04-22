50853286f506a352e11dd28659082c8badd358df
ME (Mac mini) <rxseven.com@gmail.com>
2018-04-22 12:51:31 +0000

    Create Google login component

---
+++ src/components/features/OAuth/Google/UI.js
@@ -0,0 +1,12 @@
+// Module dependencies
+import React, { Component } from 'react';
+
+// Component
+class UI extends Component {
+  render() {
+    return <div>Google Login component</div>;
+  }
+}
+
+// Module exports
+export default UI;
---
+++ src/components/features/OAuth/Google/index.js
@@ -0,0 +1,11 @@
+// Module dependencies
+import React from 'react';
+
+// Peer dependencies
+import UI from './UI';
+
+// Component
+const Google = () => <UI />;
+
+// Module exports
+export default Google;
