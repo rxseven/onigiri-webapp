c888cd32ddb289cb308c28377babc8e0f8151608
ME (Mac mini) <rxseven.com@gmail.com>
2018-04-22 08:49:16 +0000

    Add functionality to set loading status to Facebook component

--- src/components/features/OAuth/Facebook/UI.js
+++ src/components/features/OAuth/Facebook/UI.js
@@ -6,6 +6,17 @@ import { Button } from '../../../../components/shared/base/Buttons';
 
 // Component
 class UI extends Component {
+  // After a component is instantiated as well as when it receives new props...
+  static getDerivedStateFromProps(nextProps, prevState) {
+    // Set loading status
+    return { isLoading: nextProps.state.data.session.loading.signin } || null;
+  }
+
+  // Initial state
+  state = {
+    isLoading: false
+  };
+
   // Login handler
   onLogin = (response) => {
     // Success, submit the access token to the API
@@ -19,13 +30,16 @@ class UI extends Component {
 
   // Request handler
   onRequest = () => {
+    // Set loading status
+    this.setState(() => ({ isLoading: true }));
+
     // Set session status
     this.props.actions.auth.oauthRequest();
   };
 
   // Render button
   renderButton = ({ onClick }) => (
-    <Button block button="primary" handler={onClick}>
+    <Button block button="primary" disabled={this.state.isLoading} handler={onClick}>
       Login with Facebook
     </Button>
   );
