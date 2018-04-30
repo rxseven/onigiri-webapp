adad361fa636c583050052ede58ec62f2d03c225
ME (Mac mini) <rxseven.com@gmail.com>
2018-04-30 03:16:45 +0000

    Add functionality to check if the operation timed out or a network request is taking too long
    Sometimes the app has been sleeping or network connections suck and never resolve or fail, they just hang there forever. This is a bad UX because the user won't know if it should always take this long, or if they should try refreshing. The app should show a friendly message to the user.

--- src/components/core/Lightbox/UI.js
+++ src/components/core/Lightbox/UI.js
@@ -1,23 +1,60 @@
 // Module dependencies
-import React from 'react';
+import React, { PureComponent } from 'react';
 
 import Overlay from '../../shared/base/Overlay';
 import Spinner from '../../shared/base/Spinner';
+
+import asyncHelper from '../../../helpers/asynchronous';
 
 // Peer dependencies
 import styles from './styles.scss';
 
 // Component
-const UI = ({ state: { data } }) => (
-  <If condition={data.session.loading.verify}>
-    <Overlay>
-      <div className={styles.logo} />
-      <div className={styles.spinner}>
-        <Spinner loading />
-      </div>
-    </Overlay>
-  </If>
-);
+class UI extends PureComponent {
+  // Infitial state
+  state = { longLoad: false };
+
+  // After a component is mounted...
+  componentDidMount() {
+    // Check if the operation timed out or a network request is taking too long
+    this.timeoutMessage();
+  }
+
+  /**
+   * Sometimes the app has been sleeping or network connections suck and never
+   * resolve or fail, they just hang there forever. This is a bad UX because
+   * the user won't know if it should always take this long, or if they should
+   * try refreshing. The app should show a friendly message to the user.
+   */
+  timeoutMessage = () => {
+    asyncHelper.timeout(null, 5000).then((response) => {
+      this.setState(() => ({ longLoad: true }));
+    });
+  };
+
+  // Render component
+  render() {
+    // Variables
+    const { state: { data } } = this.props;
+
+    // View
+    return (
+      <If condition={data.session.loading.verify}>
+        <Overlay>
+          <div className={styles.logo} />
+          <div className={styles.spinner}>
+            <Spinner loading />
+          </div>
+          <If condition={this.state.longLoad}>
+            <div className={styles.description}>
+              <p>The app has been sleeping, it’s waking up...</p>
+            </div>
+          </If>
+        </Overlay>
+      </If>
+    );
+  }
+}
 
 // Module exports
 export default UI;
