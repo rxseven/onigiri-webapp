b48d73a9dc578eb638ce831284ce135d64fb9ba7
ME (Mac mini) <rxseven.com@gmail.com>
2018-07-01 03:28:04 +0000

    Refactor the project starting point
    - Update source comments
    - Reorder import statements
    - Add error handler for handling invalid root element

--- src/index.jsx
+++ src/index.jsx
@@ -3,20 +3,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './dependencies';
 
-// Root component
+// Starting point component
 import Root from './components/Root';
 
 // Redux store
 import configureStore from './store/configureStore';
 
-// Global styles
-import './styles/index.scss';
-
 // Constants
 import HTML from './constants/elements/html';
 
-// Create Redux store
+// Styles
+import './styles/index.scss';
+
+// Initialize Redux store
 const store = configureStore();
 
+// Initialize root HTML element
+const root = document.getElementById(HTML.root);
+
+// Validate root element
+if (root == null) {
+  throw new Error('No root element found');
+}
+
 // Render React element into the DOM
-ReactDOM.render(<Root store={store} />, document.getElementById(HTML.root));
+ReactDOM.render(<Root store={store} />, root);
