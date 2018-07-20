cd4a24099e1b298e44519f8e8eed004ede8e6d2d
ME (Mac mini) <rxseven.com@gmail.com>
2018-07-20 05:49:33 +0000

    Refactor state under screens.users.signup domain with Immutable object

--- src/screens/Users/screens/SignUp/reducers.js
+++ src/screens/Users/screens/SignUp/reducers.js
@@ -1,43 +1,44 @@
 // Module dependencies
-import { createSelector } from 'reselect';
+import { fromJS } from 'immutable';
 import { combineReducers } from 'redux-immutable';
+import { createSelector } from 'reselect';
+
+import STATE_MODELS from '../../../../constants/models/state';
+import { ERROR, LOADING } from '../../../../constants/types/asynchronous';
+import { setAsync } from '../../../../helpers/data';
 
 // Actions
 import { SIGNUP, SIGNUP_FAILURE, SIGNUP_SUCCESS } from '../../../../data/session/actions';
 import { SIGNUP_RESET_UI } from './actions';
 
-// Constants
-import STATE_MODELS from '../../../../constants/models/state';
-
 // Initial state
-const initialState = {
+const initialState = fromJS({
   post: { ...STATE_MODELS.model.asynchronous }
+});
+
+// Immutable map
+const map = {
+  post: ['post']
 };
 
 // Asynchronous reducer
 const asynchronous = (state = initialState, action) => {
-  switch (action.type) {
+  const { payload, type } = action;
+
+  switch (type) {
+    // Sign-up
     case SIGNUP:
-      return {
-        ...state,
-        post: {
-          ...initialState.post,
-          loading: true
-        }
-      };
+      return setAsync(map.post, state, LOADING);
     case SIGNUP_FAILURE:
-      return {
-        ...state,
-        post: {
-          ...initialState.post,
-          error: action.payload
-        }
-      };
+      return setAsync(map.post, state, ERROR, payload);
     case SIGNUP_SUCCESS:
+      return initialState;
+
+    // Reset state
     case SIGNUP_RESET_UI:
-      return {
-        ...initialState
-      };
+      return initialState;
+
+    // Default
     default:
       return state;
   }
@@ -50,10 +51,10 @@ const ui = combineReducers({ asynchronous });
 export default combineReducers({ ui });
 
 // Non-memoized utility selectors
-const getNode = state => state.screens.users.signup;
+const getNode = state => state.getIn(['screens', 'users', 'signup']);
 
 // Get UI state
-export const getUI = createSelector(getNode, node => node.ui);
+export const getUI = createSelector(getNode, node => node.get('ui'));
 
 // Get asynchronous state
-export const getAsync = createSelector(getNode, node => node.asynchronous);
+export const getAsync = createSelector(getNode, node => node.getIn(['ui', 'asynchronous']));
