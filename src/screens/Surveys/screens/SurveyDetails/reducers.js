b3fced4404447c9a1c1ae2c5ebd6cd73fcc123b1
ME (Mac mini) <rxseven.com@gmail.com>
2018-07-20 06:09:52 +0000

    Refactor reducer for screens.surveys.details domain with Immutable object
    - Revert commit : 93fb238c50b90a4e1299e83d526d34b5622b8f1c
    - Incorrect commit : d01fec1ef02333b525b372d4e01ec34129438188

--- src/screens/Surveys/screens/SurveyDetails/reducers.js
+++ src/screens/Surveys/screens/SurveyDetails/reducers.js
@@ -1,6 +1,6 @@
 // Module dependencies
 import { createSelector } from 'reselect';
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux-immutable';
 
 // Actions
 import { SURVEY_DELETE, SURVEY_DELETE_FAILURE, SURVEY_DELETE_SUCCESS } from '../../actions';
