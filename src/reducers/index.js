e39b3816a41e67ae8198f5bf9d7869f924ec787e
ME (Mac mini) <rxseven.com@gmail.com>
2018-07-24 03:55:20 +0000

    Refactor import statements under src/reducers with absolute paths

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,8 +4,8 @@ import { reducer as form } from 'redux-form/immutable';
 import { reducer as burgerMenu } from 'redux-burger-menu/immutable';
 
 // Reducers
-import data from '../data/reducers';
-import screens from '../screens/reducers';
+import data from 'data/reducers';
+import screens from 'screens/reducers';
 
 // Combine reducers
 const reducer = combineReducers({
