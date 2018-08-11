70b221ca78232a8c3f49a749bf9e9e5c93935f15
ME (Mac mini) <rxseven.com@gmail.com>
2018-08-11 08:03:08 +0000

    Add static types to Profile screen

--- src/screens/Users/screens/Profile/components/Checkout/index.jsx
+++ src/screens/Users/screens/Profile/components/Checkout/index.jsx
@@ -1,7 +1,6 @@
+// @flow
 // Module dependencies
-import PropTypes from 'prop-types';
-import exact from 'prop-types-exact';
-import React, { Fragment } from 'react';
+import * as React from 'react';
 
 // Components and HOCs
 import Payments from 'components/features/Payments';
@@ -11,19 +10,29 @@ import Spinner from 'components/common/Spinner';
 import Error from 'components/composite/Error';
 
 // Constants
-import PROP_TYPES from 'constants/models/propTypes';
 import STATE_MODELS from 'constants/models/state';
 
-// Declare prop types and default props
-const propTypes = exact({
-  ...PROP_TYPES.wrapper.asynchronous({
-    post: PropTypes.shape({
-      checkout: PROP_TYPES.model.asynchronous
-    })
-  }),
-  callback: PropTypes.func.isRequired
-});
+// Types
+import type { Asynchronous } from 'types/common/state';
+import type { Callback } from 'types/common/utilities';
 
+// Static types
+type Props = {
+  callback: Callback,
+  state: {
+    ui: {
+      asynchronous: {
+        post: {
+          checkout: Asynchronous
+        }
+      }
+    }
+  }
+};
+
+type Return = React.Node;
+
+// Default props
 const defaultProps = STATE_MODELS.wrapper.asynchronous({
   post: {
     checkout: { ...STATE_MODELS.model.asynchronous }
@@ -31,29 +40,26 @@ const defaultProps = STATE_MODELS.wrapper.asynchronous({
 });
 
 // Component
-const Checkout = ({ callback, state: { ui: { asynchronous } } }) => {
+const Checkout = ({ callback, state: { ui: { asynchronous } } }: Props): Return => {
   // Variables
   const { error, loading } = asynchronous.post.checkout;
 
   // View
   return (
-    <Fragment>
+    <React.Fragment>
       <CardText>Pay $5.00 get 5 survey credits.</CardText>
-      <If condition={error}>
-        <Error alert={error} />
-      </If>
+      {!!error && <Error alert={error} />}
       <ButtonSet>
         <Payments callback={callback} />
         <If condition={loading}>
           <Spinner />
         </If>
       </ButtonSet>
-    </Fragment>
+    </React.Fragment>
   );
 };
 
-// Specify prop types and default values for props
-Checkout.propTypes = propTypes;
+// Specify default values for props
 Checkout.defaultProps = defaultProps;
 
 // Module exports
