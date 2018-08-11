c2aa4a377fb8db169256859616770df0d7787e09
ME (Mac mini) <rxseven.com@gmail.com>
2018-08-11 07:10:47 +0000

    Add static types to Notification component

--- src/components/core/Notification/index.jsx
+++ src/components/core/Notification/index.jsx
@@ -1,9 +1,13 @@
+// @flow
 // Module dependencies
-import React from 'react';
+import * as React from 'react';
 import Alert from 'react-s-alert';
 
+// Static types
+type Return = React.Element<typeof Alert>;
+
 // Component
-const Notification = () => <Alert />;
+const Notification = (): Return => <Alert />;
 
 // Module exports
 export default Notification;
