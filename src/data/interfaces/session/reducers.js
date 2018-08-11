ab8604d8279ea2e97713ebbdc645336028d2e19b
ME (Mac mini) <rxseven.com@gmail.com>
2018-08-11 07:34:39 +0000

    Add static types under src/data/interfaces/session

--- src/data/interfaces/session/reducers.js
+++ src/data/interfaces/session/reducers.js
@@ -1,3 +1,4 @@
+// @flow
 // Module dependencies
 import { fromJS } from 'immutable';
 import { combineReducers } from 'redux-immutable';
@@ -10,29 +11,37 @@ import { setAsync } from 'helpers/state';
 import STATE_MODELS from 'constants/models/state';
 import { ERROR, LOADING } from 'constants/types/asynchronous';
 
-// Action types
+// Action and static types
 import { SIGNOUT_FAILURE, SIGNOUT_REQUEST, SIGNOUT_SUCCESS } from 'data/session/types';
+import type { Action } from 'data/session/types';
+import type { Asynchronous } from 'types/common/state';
 
-// Initial state
-const initialState = fromJS({
+// Static types
+type Key = Object;
+type Model = { signout: Asynchronous };
+type State = any;
+
+// State shape
+const stateShape: Model = {
   signout: { ...STATE_MODELS.model.asynchronous }
-});
+};
 
-// Immutable map
-const map = {
+// Immutable key path
+const statePath: Key = {
   signout: ['signout']
 };
 
-// Asynchronous reducer
-const asynchronous = (state = initialState, action) => {
-  const { payload, type } = action;
+// Initial state
+const initialState: State = fromJS(stateShape);
 
-  switch (type) {
+// Asynchronous reducer
+const asynchronous = (state: State = initialState, action: Action): State => {
+  switch (action.type) {
     // Sign-out
     case SIGNOUT_REQUEST:
-      return setAsync(map.signout, state, LOADING);
+      return setAsync(statePath.signout, state, LOADING);
     case SIGNOUT_FAILURE:
-      return setAsync(map.signout, state, ERROR, payload);
+      return setAsync(statePath.signout, state, ERROR, action.payload);
     case SIGNOUT_SUCCESS:
       return initialState;
 
